ff6b3eee47899f43e0b6649cf6a99ed15250f0d5
rajshares <74537738+rajshares@users.noreply.github.com>
2020-11-16 18:18:25 +0800

    Create dictionary1.py

---
+++ dictionary1.py
@@ -0,0 +1,6 @@
+Every four years, the summer Olympics are held in a different country. Add a key-value pair to the dictionary places that reflects that the 2016 Olympics were held in Brazil. Do not rewrite the entire dictionary to do this!
+
+
+ places = {"Australia":2000, "Greece":2004, "China":2008, "England":2012}
+ places ['Brazil'] = 2016
+

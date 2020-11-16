63ba6246e4c041576068d210dae33b3e14b2241a
rajshares <74537738+rajshares@users.noreply.github.com>
2020-11-16 18:43:07 +0800

    Delete dictionary2.py

--- dictionary2.py
+++
@@ -1,7 +0,0 @@
-We have a dictionary of the specific events that Italy has won medals in and the number of medals they have won for each event. Assign to the variable events a list of the keys from the dictionary medal_events. Do not hard code this.
-
-
-medal_events = {'Shooting': 7, 'Fencing': 4, 'Judo': 2, 'Swimming': 3, 'Diving': 2}
-for akey in medal_events.keys():
-        events = list(medal_events.keys())
-              

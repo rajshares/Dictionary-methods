72a5d1a34bd2de910696e1cd63f343dd066d5d7c
rajshares <74537738+rajshares@users.noreply.github.com>
2020-11-20 15:37:53 +0800

    Update whileloop.py

--- whileloop.py
+++ whileloop.py
@@ -1,7 +1,8 @@
 #Write a while loop that is initialized at 0 and stops at 15. If the counter is an even number, append the counter to a list called eve_nums.
 eve_nums = []
 calc = 0
-while int(calc) < 15:
-    eve_nums.append(calc)
-    calc = int(calc) + 2
+while (calc) <= 15:
+    if count % 2 == 0:
+     eve_nums.append(calc)
+     calc = calc + 1 
 print(eve_nums)
